9bdee04d3d6822ca2ee60ec9f17b335f6cab57f6
Mike Bostock <mbostock@gmail.com>
2016-05-13 08:48:25 -0700

    0.2.9

--- d3.geo.projection.js
+++ d3.geo.projection.js
@@ -1031,11 +1031,11 @@
   function guyou(λ, φ) {
     var k_ = (Math.SQRT2 - 1) / (Math.SQRT2 + 1), k = Math.sqrt(1 - k_ * k_), K = ellipticF(halfπ, k * k), f = -1;
     var ψ = Math.log(Math.tan(π / 4 + Math.abs(φ) / 2)), r = Math.exp(f * ψ) / Math.sqrt(k_), at = guyouComplexAtan(r * Math.cos(f * λ), r * Math.sin(f * λ)), t = ellipticFi(at[0], at[1], k * k);
-    return [ -t[1], sgn(φ) * (.5 * K - t[0]) ];
+    return [ -t[1], (φ >= 0 ? 1 : -1) * (.5 * K - t[0]) ];
   }
   function guyouComplexAtan(x, y) {
     var x2 = x * x, y_1 = y + 1, t = 1 - x2 - y * y;
-    return [ sgn(x) * π / 4 - .5 * Math.atan2(t, 2 * x), -.25 * Math.log(t * t + 4 * x2) + .5 * Math.log(y_1 * y_1 + x2) ];
+    return [ .5 * ((x >= 0 ? halfπ : -halfπ) - Math.atan2(t, 2 * x)), -.25 * Math.log(t * t + 4 * x2) + .5 * Math.log(y_1 * y_1 + x2) ];
   }
   function guyouComplexDivide(a, b) {
     var denominator = b[0] * b[0] + b[1] * b[1];
