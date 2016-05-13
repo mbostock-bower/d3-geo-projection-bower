6f8b97e3465b64c6143952f050c7596c29c9d8c4
Mike Bostock <mbostock@gmail.com>
2016-05-13 08:48:30 -0700

    0.2.12

--- d3.geo.projection.js
+++ d3.geo.projection.js
@@ -1574,6 +1574,23 @@
   (d3.geo.nellHammer = function() {
     return projection(nellHammer);
   }).raw = nellHammer;
+  var pattersonK1 = 1.0148, pattersonK2 = .23185, pattersonK3 = -.14499, pattersonK4 = .02406, pattersonC1 = pattersonK1, pattersonC2 = 5 * pattersonK2, pattersonC3 = 7 * pattersonK3, pattersonC4 = 9 * pattersonK4, pattersonYmax = 1.790857183;
+  function patterson(λ, φ) {
+    var φ2 = φ * φ;
+    return [ λ, φ * (pattersonK1 + φ2 * φ2 * (pattersonK2 + φ2 * (pattersonK3 + pattersonK4 * φ2))) ];
+  }
+  patterson.invert = function(x, y) {
+    if (y > pattersonYmax) y = pattersonYmax; else if (y < -pattersonYmax) y = -pattersonYmax;
+    var yc = y, δ;
+    do {
+      var y2 = yc * yc;
+      yc -= δ = (yc * (pattersonK1 + y2 * y2 * (pattersonK2 + y2 * (pattersonK3 + pattersonK4 * y2))) - y) / (pattersonC1 + y2 * y2 * (pattersonC2 + y2 * (pattersonC3 + pattersonC4 * y2)));
+    } while (Math.abs(δ) > ε);
+    return [ x, yc ];
+  };
+  (d3.geo.patterson = function() {
+    return projection(patterson);
+  }).raw = patterson;
   var peirceQuincuncialProjection = quincuncialProjection(guyou);
   (d3.geo.peirceQuincuncial = function() {
     return peirceQuincuncialProjection().quincuncial(true).rotate([ -90, -90, 45 ]).clipAngle(180 - 1e-6);
